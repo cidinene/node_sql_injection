0cca4ff5d10b477d5986c99c4bcbab0b2b88bc30
Irene <cidirene@gmail.com>
2021-07-13 16:13:29 +0200

    adding form to launch

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,15 @@
+//Dependencies
 var express = require('express');
 var router = express.Router();
-
 var fs = require("fs");
 var file = "./db/chinook.db";
 var exists = fs.existsSync(file);
 const sqlite3 = require('sqlite3').verbose();
 var db;
 const LOGIN_ERROR = "-1"
+
+
+// Database access as variable...it should be close somewhere
 db =  new sqlite3.Database('./db/chinook.db', sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
       console.error(err.message);
@@ -16,6 +19,8 @@ db =  new sqlite3.Database('./db/chinook.db', sqlite3.OPEN_READWRITE, (err) => {
  
 );
 
+
+//testing access to database without params
 async function getPlayList(){
  const res = await new Promise((resolve, reject) => {
     db.all('SELECT  playlist_id , name FROM playlist', [], (err, row) => {
@@ -37,6 +42,7 @@ return res;
 }
 
 
+//function where parameters are not cheked and injected in the sql query directly
 async function login(name,pwd){
   const res = await new Promise((resolve, reject) => {
      db.all("SELECT  * FROM employee where first_name like '"+name+"' and last_name like'"+pwd+"'", [], (err, row) => {
@@ -58,14 +64,15 @@ async function login(name,pwd){
  return res;
  }
 
-/* GET palylist listing. */
-router.get('/', async function(req, res, next) {
+// entry to users */users/  route
+router.all('/', async function(req, res, next) {
   var playlist = await getPlayList()
   res.send("<html><body>"+playlist+"</body></html>");
   
 });
 
-router.get('/login', async function(req, res, next) {
+// entry to users */users/login  route
+router.all('/login', async function(req, res, next) {
   let name=req.query.name||"";
   let pwd =req.query.pwd ||""; 
   if (name.length>0&&pwd.length>0)
